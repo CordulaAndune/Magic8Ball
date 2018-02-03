e5f646c986c8ffa33acaef049640df63b008d4e5
Cordula <cordula.gloge@gmx.de>
2018-02-03 11:30:02 +0100

    Initial commit

---
+++ app/src/main/java/de/cordaelproductions/cg/getyouranswer/MainActivity.java
@@ -0,0 +1,122 @@
+package de.cordaelproductions.cg.getyouranswer;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SensorManager mSensorManager;
+    private Boolean hasAccelerometer;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
+    private TextView answerTextView;
+    private ImageView answerImageView;
+    private int[][] allAnswers;
+    private int oldAnswer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        oldAnswer = -1;
+
+        // get TextView and ImageView
+        answerTextView = findViewById(R.id.answerText);
+        answerImageView = findViewById(R.id.answerImage);
+
+        allAnswers = new int[][]{{R.string.yes, R.drawable.yes},
+                {R.string.no, R.drawable.no},
+                {R.string.dont_count, R.drawable.dont_count},
+                {R.string.source_no, R.drawable.source_no},
+                {R.string.better_not_tell, R.drawable.better_not_tell},
+                {R.string.meh, R.drawable.meh},
+                {R.string.ask_later, R.drawable.ask_later},
+                {R.string.doubt_it, R.drawable.doubt_it},
+                {R.string.without_doubt, R.drawable.without_doubt},
+                {R.string.certain, R.drawable.certain},
+                {R.string.definitely, R.drawable.definitly},};
+
+
+        // ShakeDetector initialization
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        hasAccelerometer = getPackageManager().hasSystemFeature(PackageManager.FEATURE_SENSOR_ACCELEROMETER);
+        if (hasAccelerometer) {
+            answerTextView.setText(R.string.shake);
+            mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            mShakeDetector = new ShakeDetector();
+            mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+                @Override
+                public void onShake(long Count) {
+                    chooseAnswer();
+                }
+            });
+        } else {
+            answerTextView.setText(R.string.click_button);
+            answerImageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    chooseAnswer();
+                }
+            });
+        }
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // register the Session Manager Listener onResume
+        if (hasAccelerometer) {
+            mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        // unregister the Sensor Manager onPause
+        if (hasAccelerometer) {
+            mSensorManager.unregisterListener(mShakeDetector);
+        }
+        super.onPause();
+    }
+
+    /**
+     * Choose the answer by selecting a randomNumber as index of the answer in the answer Array
+     */
+    private void chooseAnswer() {
+        int randomAnswer = setRandomNumber();
+        answerTextView.setText(allAnswers[randomAnswer][0]);
+        answerImageView.setImageResource(allAnswers[randomAnswer][1]);
+        oldAnswer = randomAnswer;
+    }
+
+    /**
+     * select a random Number, avoid the same answer two times in series by recursion
+     *
+     * @return random number
+     */
+    private int setRandomNumber() {
+        int randomAnswer;
+        Random random = new Random(System.currentTimeMillis());
+        randomAnswer = (random.nextInt(allAnswers.length));
+        if (randomAnswer == oldAnswer) {
+            return setRandomNumber();
+        } else {
+            return randomAnswer;
+        }
+    }
+
+
+}
---
+++ app/src/main/java/de/cordaelproductions/cg/getyouranswer/ShakeDetector.java
@@ -0,0 +1,76 @@
+package de.cordaelproductions.cg.getyouranswer;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+
+/**
+ * Detection of the device shake
+ * <p>
+ * based on jasonmcrevnolds.com/?p=388
+ * Created by Cordula Gloge on 26/01/2018.
+ */
+
+public class ShakeDetector implements SensorEventListener {
+
+    private static final float SHAKE_THRESHOLD_GRAVITY = 2.7F;
+    private static final int SHAKE_SLOP_TIME_MS = 500;
+    private static final int SHAKE_COUNT_RESET_TIME_MS = 2000;
+
+    private OnShakeListener mListener;
+    private long mShakeTimestamp;
+    private long mShakeCount;
+
+    public void setOnShakeListener(OnShakeListener listener) {
+        this.mListener = listener;
+    }
+
+    public interface OnShakeListener {
+        void onShake(long count);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //ignore
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if (mListener != null) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            float gX = x / SensorManager.GRAVITY_EARTH;
+            float gY = y / SensorManager.GRAVITY_EARTH;
+            float gZ = z / SensorManager.GRAVITY_EARTH;
+
+            //gForce will be close to 1 when there is no movement
+            float gForce = (float) Math.sqrt(gX * gX + gY * gY + gZ * gZ);
+            if (gForce > SHAKE_THRESHOLD_GRAVITY) {
+                final long now = System.currentTimeMillis();
+                // reset the shake count after 3 seconds of no shakes
+                if (mShakeTimestamp + SHAKE_COUNT_RESET_TIME_MS < now) {
+                    mShakeCount = 0;
+                }
+
+                // ignore shake events too close to each other (500 ms) and mShakeCount > 1.0
+                if (mShakeTimestamp + SHAKE_SLOP_TIME_MS < now) {
+                    if (mShakeCount > 1.0) {
+                        return;
+                    }
+                }
+
+                mShakeTimestamp = now;
+                mShakeCount++;
+                // Execute onShake only one time per shake series
+                if (mShakeCount == 1.0) {
+                    mListener.onShake(mShakeCount);
+                }
+            }
+        }
+    }
+}
